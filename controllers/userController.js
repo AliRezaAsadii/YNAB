28928cab8082fd3229724f0da9b808588061dd83
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-05-24 01:19:51 +0330

    create userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//* imports
+
+const User = require('./../models/userModel')
+
+//* functions
+//! not finished in developing...
+
+exports.getAllUsers = async (req , res) => {}
+
+exports.getUser = async (req , res) => {}
+
+exports.createUser = async (req , res) => {}
+
+exports.updateUser = async (req , res) => {}
+
+exports.deleteUser = async (req , res) => {}
