2b1a7784df1ac3069cb1c7f357b789aed4fe7f7b
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-05-24 00:15:42 +0330

    install packages

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-"use strict"
+"use strict";
+
+//* packages
+
+const express = require("express");
+const morgan = require("morgan")
+
+//* middleware
+
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan("dev"))
+
+//* exports
+
+module.exports = app;
