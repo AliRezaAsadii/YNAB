d8fa46ce7caf0a18b8776f6cc1732def4c376fd4
AliRezaAsadii <0alirezasadi0@gmail.com>
2024-05-31 01:19:31 +0330

    create walletController

---
+++ controllers/walletController.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const Wallet = require("./../models/walletModel");
+
+exports.getWallet = async (req, res) => {
+  try {
+    const wallet = await Wallet.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: wallet,
+    });
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+exports.createWallet = async (req, res) => {
+  const newWallet = await Wallet.create(req.body);
+
+  try {
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newWallet,
+      },
+    });
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+exports.updateWallet = async (req, res) => {
+  try {
+    const wallet = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      requestAt: req.requestTime,
+      data: {
+        wallet,
+      },
+    });
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+exports.deleteWallet = async (req, res) => {
+  try {
+    await Wallet.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      requestAt: req.requestTime,
+      massage: "successful",
+    });
+  } catch {
+    res.status(400).send(err);
+  }
+};
+
